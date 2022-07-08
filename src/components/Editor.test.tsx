93c9e4f9a3143290ca1e2ff4dbe5d54af430cdd9
Thomas Eizinger <thomas@eizinger.io>
2022-07-08 10:03:58 +0200

    Add a few UI tests

---
+++ src/components/Editor.test.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Editor from './Editor';
+
+test('UI renders table headers', () => {
+  render(<LocalStateEditor />);
+
+  expect(
+    screen.getByRole('columnheader', { name: 'Project' })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole('columnheader', { name: 'Start' })
+  ).toBeInTheDocument();
+  expect(screen.getByRole('columnheader', { name: 'End' })).toBeInTheDocument();
+  expect(
+    screen.getByRole('columnheader', { name: 'Duration' })
+  ).toBeInTheDocument();
+});
+
+test('UI renders start but not stop button', () => {
+  render(<LocalStateEditor />);
+
+  let startButton = screen.getByRole('button', { name: 'Start' });
+
+  expect(startButton).toBeInTheDocument();
+});
+
+test('UI renders stop button after clicking', () => {
+  render(<LocalStateEditor />);
+
+  let startButton = screen.getByRole('button', { name: 'Start' });
+
+  fireEvent.click(startButton);
+
+  let stopButton = screen.getByRole('button', { name: 'Stop' });
+
+  expect(stopButton).toBeInTheDocument();
+});
+
+function LocalStateEditor() {
+  let [note, saveNote] = useState('');
+
+  return <Editor note={note} saveNote={saveNote} />;
+}
